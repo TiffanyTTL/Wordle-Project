bdae744d08219d0459f6357066d10b67dcb63847
Tobi-Lawal, Boluwatife (Capgemini) <tiffany.tobi-lawal@capgemini.com>
2023-02-23 20:03:30 +0000

    Logic for creating a user - using only user email address.

--- src/main/java/com/example/Wordle/Project/service/UserService.java
+++ src/main/java/com/example/Wordle/Project/service/UserService.java
@@ -1,4 +1,33 @@
 package com.example.Wordle.Project.service;
 
+import com.example.Wordle.Project.model.User;
+import com.example.Wordle.Project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * User service class.
+ */
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    /**
+     * constructor generated for the user service class.
+     */
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * create user method class.
+     */
+    public  String createUser(User user) {
+        userRepository.insert(user);
+        return "user has been created";
+
+    }
 }
